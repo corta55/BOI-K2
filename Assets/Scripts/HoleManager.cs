c95844515b709686b8ad8693e5408e0814b29f81
Abdul Yasir Halomoan <smpyasir01@gmail.com>
2018-06-02 00:38:15 +0700

    crouching and teleporting

---
+++ Assets/Scripts/HoleManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class HoleManager : MonoBehaviour {
+
+    public Transform nextHole;
+    public Transform player;
+    
+    public void goesThrough()
+    {
+        player.position = new Vector2 (nextHole.position.x,nextHole.position.y);
+        DelayBeforeEnteringAgain(.5f);
+    }
+
+    public static void DelayBeforeEnteringAgain(float seconds)
+    {
+        DateTime dt = DateTime.Now + TimeSpan.FromSeconds(seconds);
+
+        do { } while (DateTime.Now < dt);
+    }
+
+}

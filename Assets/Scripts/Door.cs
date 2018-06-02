dbc19e802e03c4ee016e6797289e07433e4a80be
Ari Utomo <robert.unix98@gmail.com>
2018-06-02 09:10:15 -0700

    Update

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactable{
+
+    [SerializeField] Transform tujuanPintu;
+    Transform player;
+    [SerializeField] private bool haveRequire;
+    public bool grantedAccess;
+
+	public override void interact()
+    {
+        GameVariables.isInteract = true;
+        player = GameVariables.activePlayer.transform;
+        if (haveRequire && !grantedAccess) return;
+        StartCoroutine(delayMasuk());
+    }
+
+    IEnumerator delayMasuk()
+    {
+        //For animation in Door
+        yield return new WaitForSeconds(1f);
+        player.GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        player.position = tujuanPintu.position;
+        yield return new WaitForSeconds(1f);
+        player.GetComponent<SpriteRenderer>().enabled = true;
+        //For animation out Door
+        yield return new WaitForSeconds(1f);
+        GameVariables.isInteract = false;
+    }
+}
--- Assets/Scripts/GameVariables.cs
+++ Assets/Scripts/GameVariables.cs
@@ -1,8 +1,12 @@
-﻿public static class GameVariables
+﻿using UnityEngine;
+
+public static class GameVariables
 {
     public static bool boundingCam = true;
     public static string followingPlayer = "Player";
 
-    public static bool isCrouching;
+    public static bool isInteract;
+
+    public static GameObject activePlayer;
 
 }

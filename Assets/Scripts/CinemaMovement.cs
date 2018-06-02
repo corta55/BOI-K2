34b42cf808c3876dcda52afc02beec1cc2b403bb
Ari Utomo <robert.unix98@gmail.com>
2018-06-01 23:34:27 -0700

    Fixed Moving Camera

--- Assets/Scripts/CinemaMovement.cs
+++ Assets/Scripts/CinemaMovement.cs
@@ -10,8 +10,6 @@ public class CinemaMovement : MonoBehaviour {
     private Vector2 posCam;
     Vector3 newPosition;
 
-    private bool freezeCam;
-
     #region Bounding Cam
 
         #region SizeCam
@@ -36,7 +34,7 @@ public class CinemaMovement : MonoBehaviour {
 
     #endregion
 
-    void FixedUpdate()
+    void Update()
     {
         followingPlayer = GameVariables.followingPlayer;
         posCam = transform.position;
@@ -49,31 +47,30 @@ public class CinemaMovement : MonoBehaviour {
             {
                 if (posCam.x + right >= rightBound.position.x && newPosition.x + right >= rightBound.position.x)
                 {
-                    freezeCam = true;
+                    posCam.x = rightBound.position.x - right;
+                    newPosition.x = rightBound.position.x - right;
                 }
 
                 if (posCam.x - left <= leftBound.position.x && newPosition.x - left <= leftBound.position.x)
                 {
-                    freezeCam = true;
+                    posCam.x = leftBound.position.x + left;
+                    newPosition.x = leftBound.position.x + left;
                 }
 
                 if (posCam.y + up >= upBound.position.y && newPosition.y + up >= upBound.position.y)
                 {
-                    freezeCam = true;
+                    posCam.y = upBound.position.y - up;
+                    newPosition.y = upBound.position.y - up;
                 }
 
                 if (posCam.y - down <= downBound.position.y && newPosition.y - down <= downBound.position.y)
                 {
-                    freezeCam = true;
+                    posCam.y = downBound.position.y + down;
+                    newPosition.y = downBound.position.y + down;
                 }
             }
 
-            if (newPosition.x + right < rightBound.position.x && newPosition.x - left > leftBound.position.x && newPosition.y + up < upBound.position.y && newPosition.y - down > downBound.position.y)
-            {
-                freezeCam = false;
-            }
-
-            if ((posCam.x + right != rightBound.position.x || posCam.x - left != leftBound.position.x || posCam.y + up != upBound.position.y || posCam.y - down != downBound.position.y) && !freezeCam)
+            if (posCam.x + right != rightBound.position.x || posCam.x - left != leftBound.position.x || posCam.y + up != upBound.position.y || posCam.y - down != downBound.position.y)
             {
                 transform.position = Vector3.Lerp(posCam, newPosition, speedFollowing * Time.deltaTime);
             }
---
+++ Assets/Scripts/Cobaaja.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Cobaaja : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left*10*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 10 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * 10 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * 10 * Time.deltaTime);
+        }
+    }
+}
